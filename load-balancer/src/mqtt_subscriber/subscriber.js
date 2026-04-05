5468b8c47a74b85acb5e63559dfa589f3fa38c2a
nxhhoang <hoangbinhthuong@gmail.com>
2026-04-05 12:38:10 +0700

    subscriber server

---
+++ load-balancer/src/mqtt_subscriber/subscriber.js
@@ -0,0 +1,71 @@
+const mqtt = require('mqtt')
+const http = require('http')
+const fs = require('fs')
+const path = require('path');
+const { timeStamp } = require('console');
+
+const configPath = path.join(__dirname, '../../config/upstream.conf');
+const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
+const host = config.adafruit_io.host
+const topics = config.adafruit_io.topics
+
+console.log(config)
+
+const options = {
+    username: config.adafruit_io.username,
+    password: config.adafruit_io.key
+}
+
+const mqtt_client = mqtt.connect(host, options)
+
+mqtt_client.on('connection', () => {
+    console.log('MQTT ok')
+
+    topics.forEach(topic => {
+        mqtt_client.subscribe(topic, (err) => {
+            if (!err) {
+                console.log(topic, " subscribe ok")
+
+
+            }
+        })
+    });
+})
+
+mqtt_client.on('message', (topic, message) => {
+    const payload = message.toString()
+    console.log(`Nhận được từ ${topic}: ${payload}`)
+
+    const sensorName = topic.split('.')[1];
+    const jsonSensor = {
+        sensor: sensorName, 
+        value: payload,
+        timestamp: Date.now()
+    }
+
+    const data = JSON.stringify(jsonSensor)
+
+    const port_options = {
+        host: '127.0.0.1',
+        port: config.http_port,
+        path: 'api/v1/internal/telemetry',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    }
+
+    const req = http.request(port_options, (res) => {
+        if (res.statusCode === 200 || res.statusCode === 201) {
+            console.log(`[MQTT Subscriber] Đã chuyển tiếp dữ liệu ${sensorType} thành công.`);
+        }
+    })
+
+    req.on('error', (e) => {
+        console.error(`[MQTT Subscriber] Lỗi khi chuyển tiếp dữ liệu: ${e.message}`);
+    });
+
+    res.write(data)
+    res.end()
+})
